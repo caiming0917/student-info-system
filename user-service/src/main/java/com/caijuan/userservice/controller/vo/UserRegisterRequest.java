05bc43776a542f8a4d59768935bf4d5bf1405862
dandelionstar <2260938536@qq.com>
2023-03-02 22:12:36 +0800

    初始化项目

--- user-service/src/main/java/com/caijuan/userservice/controller/vo/UserRegisterRequest.java
+++ user-service/src/main/java/com/caijuan/userservice/controller/vo/UserRegisterRequest.java
@@ -1,8 +1,13 @@
 package com.caijuan.userservice.controller.vo;
 
+import com.caijuan.commons.common.ErrorCode;
+import com.caijuan.commons.exception.BusinessException;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.Serializable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Data
 public class UserRegisterRequest implements Serializable {
@@ -14,4 +19,30 @@ public class UserRegisterRequest implements Serializable {
     private String userPassword;
 
     private String checkPassword;
+
+    public static void checkUserAccount(UserRegisterRequest user) {
+        String account = user.getUserAccount().trim();
+        String password =  user.getUserAccount().trim();
+        String checkPassword = user.getUserAccount().trim();
+        if (StringUtils.isAnyBlank(account, password, checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if (account.length() < 4) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账号过短");
+        }
+        if (password.length() < 8 || checkPassword.length() < 8) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
+        }
+        // 账户不能包含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(account);
+        if (matcher.find()) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账户不能包含特殊字符");
+        }
+        // 密码和校验密码相同
+        if (!password.equals(checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "两次输入密码不一致");
+        }
+    }
+
 }
--- user-service/src/main/java/com/caijuan/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/caijuan/userservice/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.caijuan.userservice.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.caijuan.commons.common.ErrorCode;
+import com.caijuan.commons.exception.BusinessException;
 import com.caijuan.userservice.controller.vo.UserRegisterRequest;
 import com.caijuan.userservice.entity.User;
 import com.caijuan.userservice.mapper.UserMapper;
 import com.caijuan.userservice.service.UserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.caijuan.userservice.controller.vo.UserRegisterRequest.checkUserAccount;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author cai juan
@@ -18,9 +29,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
+    private static final String SALT = "cai_juan";
+    @Resource
+    private UserMapper userMapper;
+
     @Override
     public long userRegister(UserRegisterRequest registerRequest) {
-
-        return 0;
+        // 1. 校验
+        checkUserAccount(registerRequest);
+        String account = registerRequest.getUserAccount().trim();
+        String password = registerRequest.getUserAccount().trim();
+        // 账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", registerRequest.getUserAccount());
+        long count = userMapper.selectCount(queryWrapper);
+        if (count > 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号重复");
+        }
+        // 2. 加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + password).getBytes());
+        // 3. 插入数据
+        User user = new User();
+        user.setAccount(account);
+        user.setPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            return -1;
+        }
+        return user.getId();
     }
 }
